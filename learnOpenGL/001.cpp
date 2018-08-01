aab8a7cce58e73387b2b9b611ca63b00d835453e
naizicqc <910985309@qq.com>
2018-08-01 10:04:54 +0800

    Create a window

--- learnOpenGL/001.cpp
+++ learnOpenGL/001.cpp
@@ -4,6 +4,9 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height); //每次窗口大小被调整时,调整视口大小
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
 int main()
 {
 	glfwInit();
@@ -12,7 +15,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //使用核心模式
 
 	//创建窗口
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -20,6 +23,7 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); //注册回调函数：每次窗口大小被调整时,调整视口大小
 
 	//初始化GLAD，用于管理OpenGL的函数指针
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
@@ -28,21 +32,19 @@ int main()
 		return -1;
 	}
 
-	//设置Viewport(视口：渲染窗口的大小)，
-	glViewport(0, 0, 800, 600);  //前两个参数控制窗口左下角的位置，后两个参数控制窗口的宽度和高度。
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); //注册回调函数：每次窗口大小被调整时,调整视口大小
-
 	//渲染循环
 	while (!glfwWindowShouldClose(window))
 	{
-		glfwSwapBuffers(window); //交换颜色缓冲
-		glfwPollEvents(); //检测触发事件
+		glfwSwapBuffers(window); //交换颜色缓冲,它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上。
+		glfwPollEvents(); //检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数
 	}
 
+	glfwTerminate(); //释放资源 
 	return 0;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-	glViewport(0, 0, width, height);
+	//设置Viewport(视口：渲染窗口的大小)
+	glViewport(0, 0, width, height); //前两个参数控制窗口左下角的位置，后两个参数控制窗口的宽度和高度。
 }
