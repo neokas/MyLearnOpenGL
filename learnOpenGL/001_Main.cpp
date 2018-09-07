d6cd33050d8b6eef5ec8eb2131eed9450717a036
naizicqc <910985309@qq.com>
2018-09-07 11:07:45 +0800

    纹理1

--- learnOpenGL/001_Main.cpp
+++ learnOpenGL/001_Main.cpp
@@ -1,23 +1,16 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
+#include <stb_image.h>
 
 #include "MyShader.h"
+
+#include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height); //每次窗口大小被调整时,调整视口大小
 void processInput(GLFWwindow *window);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
-//const char *fragmentShaderSource2 = 
-//	"#version 330 core\n"
-//	"out vec4 FragColor;\n"
-//	"int vec4 ourColor;\n"
-//	"void main()\n"
-//	"{\n"
-//	"   FragColor = vec4(ourColor,1.0);\n"
-//	"}\n\0";
 
 int main()
 {
@@ -46,30 +39,66 @@ int main()
 
 	//顶点
 	float vertices[] = {
-		// positions         // colors
-		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+		// 位置                // 颜色           // 纹理坐标
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+	};
 
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
 	};
 
 	//创建VBO VAO
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-
+	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO); //绑定VAO
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);   //绑定缓冲VBO到GL_ARRAY_BUFFER上  顶点缓冲对象的缓冲类型是GL_ARRAY_BUFFER
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //复制顶点数据 到 缓冲内存中
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	//位置信息
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	//颜色信息
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	//纹理坐标信息
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	Shader myShader("E:/桌面/learnopengl/Shaders/1.vs", "E:/桌面/learnopengl/Shaders/1.fs");
+
+	//纹理
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// 为当前绑定的纹理对象设置环绕、过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// 加载并生成纹理
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load("E:/桌面/learnopengl/images/container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);//纹理
+		glGenerateMipmap(GL_TEXTURE_2D); //自动生成多级渐远纹理
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data); //生成了纹理和相应的多级渐远纹理后，释放图像的内存
+
+	
 
 	//渲染循环
 	while (!glfwWindowShouldClose(window))
@@ -83,28 +112,26 @@ int main()
 
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //线框模式
 		
-
+	    glActiveTexture(GL_TEXTURE0); //在绑定纹理之前先激活纹理单元
+		glBindTexture(GL_TEXTURE_2D, texture);
 
 		myShader.use();
 		glBindVertexArray(VAO);
-		float offset = 0;
-		myShader.setFloat("xOffset", offset);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
 
 
 		//检查并调用事件，交换缓冲；
-		glfwPollEvents(); //检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数
 		glfwSwapBuffers(window); //交换颜色缓冲,它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上。
-	
+		glfwPollEvents(); //检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数
+
 	}
 
 	//删除VAO VBO
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-
+	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate(); //释放资源 
 	return 0;
